96066024f84b671f6306dd3d61fca4103da7306f
mousanageh <200moussa200@gmail.com>
2023-03-27 18:48:30 +0200

    init django app with docker , env files , and base commands for create admin

---
+++ backend/crud_note/base_commands/management/commands/init_admin.py
@@ -0,0 +1,18 @@
+from django.contrib.auth import get_user_model as user
+from django.core.management.base import BaseCommand, CommandError
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        try :
+            created_user,created = user().objects.get_or_create(
+                email='admin@admin.com',
+                is_staff=True,
+                is_superuser=True,
+                is_active=True
+            )
+            if created:
+                created_user.set_password('12345')
+                created_user.save()
+            print("current admin : ", created_user)
+        except Exception as e:
+            raise CommandError(e)
